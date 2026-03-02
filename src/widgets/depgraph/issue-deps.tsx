f64a20c11cc5581092efac7d61ff22847ca62d0e
Max Moldmann <Max.Moldmann@emerson.com>
2026-03-02 12:17:20 -0600

    Horizontal edge labels

--- src/widgets/depgraph/issue-deps.tsx
+++ src/widgets/depgraph/issue-deps.tsx
@@ -158,6 +158,7 @@ const IssueDeps: React.FunctionComponent<IssueDepsProps> = ({
     hierarchical: DEFAULT_USE_HIERARCHICAL_LAYOUT,
     hierarchicalDirection: "TB",
     alternateTreeLayout: DEFAULT_USE_ALTERNATE_TREE_LAYOUT,
+    horizontalEdgeLabels: false,
   });
   const [fieldInfo, setFieldInfo] = useState<FieldInfo>({});
   const [issueData, setIssueData] = useState<{ [key: string]: IssueInfo }>({});
@@ -439,6 +440,7 @@ const IssueDeps: React.FunctionComponent<IssueDepsProps> = ({
               maxNodeWidth={maxNodeWidth}
               useHierarchicalLayout={layoutOptions.hierarchical}
               useAlternateTreeLayout={layoutOptions.alternateTreeLayout}
+              horizontalEdgeLabels={layoutOptions.horizontalEdgeLabels}
               followUpstream={followUpstream}
               followDownstream={followDownstream}
               showNodeLabelFlags={nodeLabelOptions.showFlags}
@@ -452,6 +454,9 @@ const IssueDeps: React.FunctionComponent<IssueDepsProps> = ({
               }
               setUseAlternateTreeLayout={(alternateTreeLayout: boolean) =>
                 setLayoutOptions((prev) => ({ ...prev, alternateTreeLayout }))
+              }
+              setHorizontalEdgeLabels={(horizontalEdgeLabels: boolean) =>
+                setLayoutOptions((prev) => ({ ...prev, horizontalEdgeLabels }))
               }
               setFollowUpstream={setFollowUpstream}
               setFollowDownstream={setFollowDownstream}
@@ -519,6 +524,18 @@ const IssueDeps: React.FunctionComponent<IssueDepsProps> = ({
                     Alternate tree layout
                   </Toggle>
                 )}
+                <Toggle
+                  size={ToggleSize.Size14}
+                  checked={layoutOptions.horizontalEdgeLabels}
+                  onChange={(e: any) =>
+                    setLayoutOptions((prev) => ({
+                      ...prev,
+                      horizontalEdgeLabels: e.target.checked,
+                    }))
+                  }
+                >
+                  Horizontal edge labels
+                </Toggle>
                 {layoutOptions.hierarchical && (
                   <Select
                     data={treeDirectionSelectItems}
